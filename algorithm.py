c67a1437dc997cf44c7353d8f91717aabac7d6da
Puria Radmard <pr450@cam.ac.uk>
2019-07-17 20:54:14 +0100

    import numpy as np

---
+++ algorithm.py
@@ -0,0 +1,190 @@
+import numpy as np
+from tkinter import *
+
+class Node:
+    
+    def __init__(self, name):
+        self.name        = name
+        self.connections = {}
+        self.tent        = False
+        self.x = None
+        self.y = None
+        
+    
+    def __repr__(self):
+        return(self.name)
+
+    def draw(self, can, x, y):
+        self.x, self.y = x, y
+        self.avi  = can.create_oval(x-10, y-10, x+10, y+10, fill="red", outline="red")
+        self.text = can.create_text(x + 15 , y + 15, text=self.name, fill = "red")
+        return (self.avi, self.text)
+
+    def delete(self, can):
+        can.delete(self.avi)
+        can.delete(self.text)
+
+
+            
+class Graph:
+    
+    def __init__(self, name, nodes):
+        self.name  = name
+        self.nodes = nodes
+        
+    def __repr__(self):
+
+        names = []
+        for node in self.nodes:
+            names.append(node.name)
+
+        return("Graph, {}, contains nodes: {}".format(self.name, names))
+            
+    def add_node(self, node):
+        self.nodes.append(node)
+    
+    def delete_node(self, node):
+        self.nodes.remove(node)
+
+    def form_connection(self, node1, node2, weight):
+        node1.connections[node2] = [weight, False]        
+        node2.connections[node1] = [weight, False]
+    
+    def delete_connection(self, node1, node2):
+        if node1 in node2.connections:
+            node1.connections.pop(node2)
+            node2.connections.pop(node1)
+        else:
+            pass
+        
+    def purge_connections(self, node):
+        for connection in node.connections:
+            node.connections[connection][1] = False
+            connection.connections[node][1] = False
+    
+    def purge_all_connections(self):
+        for node in self.nodes:
+            self.purge_connections(node)
+                 
+    def multi_split_path(self, path, splits):
+        outpaths = []
+        for splitnode in splits:
+            outpaths.append(path + [splitnode])
+        return(outpaths)
+    
+    def multi_split_path_self(self, paths1, path1, splits1):
+        if splits1 == []:
+            pass
+        else:
+            paths1.remove(path1)
+            for splitnode in splits1:
+                paths1.append(path1 + [splitnode])
+            
+    def list_paths(self, start, end):
+        
+        paths = [[start]]
+        next_nodes = []
+        paths_temp = []
+        
+        while True: 
+            
+            paths_temp = paths 
+            for path in paths:
+                for connection in path[-1].connections:
+                    if (path[-1].connections[connection][1]) and (connection.tent == path[-1].tent + path[-1].connections[connection][0]):
+                        next_nodes.append(connection)
+                        
+                self.multi_split_path_self(paths_temp, path, next_nodes)
+                next_nodes = []
+            
+            paths = paths_temp
+            n = 0
+            for path in paths:
+                if path[-1] == end:
+                    n += 1
+            if n == len(paths):
+                return(paths)  
+        
+    def dijkstras(self, start, end):
+        
+        assert start in self.nodes
+        assert end   in self.nodes
+        start.tent = 0 
+        nodes_in_use = [start]
+        next_niu = []
+        number = 0
+        
+        while number < len(end.connections): 
+
+            if end in nodes_in_use:
+                number += 1
+
+            for node in nodes_in_use:
+                for partner in node.connections:
+                    if ((node.tent + node.connections[partner][0] < partner.tent) or (partner.tent is False)):                         
+                        partner.tent = node.tent + node.connections[partner][0]
+                        next_niu.append(partner)                        
+                    else:
+                        next_niu.append(partner) 
+                        
+            nodes_in_use = next_niu
+            next_niu = []
+        
+        nodes_in_use = [end]
+        next_niu = []
+        
+        while True:
+            for node in nodes_in_use:
+                for partner in node.connections:
+                    if (partner.tent == node.tent - node.connections[partner][0]):
+                        next_niu.append(partner)
+                        node.connections[partner][1] = True
+                        partner.connections[node][1] = True
+            nodes_in_use = next_niu
+            next_niu = []
+            if nodes_in_use == []:
+                break
+        
+        path_length = end.tent
+        pathsu = self.list_paths(start, end)
+        pathss = sorted(pathsu, key = lambda path: len(path))
+        
+        return (path_length, pathss)
+    
+#test
+    ##########################################################################################
+
+
+
+    #a = Node("NodeA")
+    #b = Node("NodeB")
+    #c = Node("NodeC")
+    #d = Node("NodeD")
+    #e = Node("NodeE")
+    #f = Node("NodeF")
+    #g = Node("NodeG")
+    #h = Node("NodeH")
+    #i = Node("NodeI")
+    #j = Node("NodeJ")
+
+    #graph1 = Graph("Graph 1", (a, b, c, d, e, f, g, h, i, j))
+
+    #graph1.form_connection(a, b, 100)
+    #graph1.form_connection(a, c, 2)
+    #graph1.form_connection(f, c, 100)
+    #graph1.form_connection(f, g, 100)
+    #graph1.form_connection(d, g, 16)
+    #graph1.form_connection(h, g, 4)
+    #graph1.form_connection(j, g, 100)
+    #graph1.form_connection(h, i, 4)
+    #graph1.form_connection(j, b, 100)
+    #graph1.form_connection(i, b, 4)
+    #graph1.form_connection(d, b, 4)
+    #graph1.form_connection(e, b, 100)
+    #graph1.form_connection(d, e, 1)
+    #graph1.form_connection(c, d, 2)
+    #graph1.form_connection(c, e, 1)
+
+
+    #print(graph1.dijkstras(a, g))
+
---
+++ gui.py
@@ -0,0 +1,236 @@
+from algorithm import Graph, Node
+from tkinter import *
+import numpy as np
+
+
+
+class DijkApp:
+
+    def __init__(self, window):
+        self.drnb   = Button(window, text = "Draw Node", command = self.draw_node)
+        self.denb   = Button(window, text = "Delete Node", command = self.delete_node)
+        self.drne   = Button(window, text = "Draw Edge", command = self.draw_edge)
+        self.dene   = Button(window, text = "Delete Edge", command = self.delete_edge)
+        self.canbut = Button(window, text = "Cancel", command = self.cancel_command)
+        self.delall = Button(window, text = "Delete All", command = self.delete_all)
+        self.canvas = Canvas(window, width = 600, height = 500, bg = "black")
+        self.action = 0
+        self.graph  = Graph("MyGraph", [])
+        self.edges  = []
+        self.window = window
+        self.counter = 0
+        self.sel     = None
+        self.don     = None
+        self.upwidge = []
+
+    def clear_widge(self):
+        for item in self.upwidge:
+            item.grid_forget()
+        self.upwidge = []
+
+    def user_action(self, event):
+        
+        if self.action == 0:
+            pass
+        
+        elif self.action == 1:
+
+            x = event.x
+            y = event.y
+
+            in_name = StringVar()
+
+            in_name_label = Label(self.window, text = "Node Name:", fg = "red")
+            in_name_label.grid(row = 2, column = 5)
+            self.upwidge.append(in_name_label)
+
+            in_name_entry = Entry(self.window)
+            in_name_entry.grid(row = 3, column = 5)
+            self.upwidge.append(in_name_entry)
+
+            in_name_button = Button(self.window, text = "Submit", command = lambda: in_name.set( in_name_entry.get() ) )
+            in_name_button.grid(row = 4, column = 5)
+            self.upwidge.append(in_name_button)
+            in_name_button.wait_variable(in_name)
+
+            globals()["node{}".format(self.counter)] = Node( in_name.get() )
+            globals()["node{}".format(self.counter)].draw(self.canvas, x, y)
+            self.graph.nodes.append( globals()["node{}".format(self.counter)] )
+            self.counter += 1
+
+            self.clear_widge()
+            self.action = 0
+        
+        elif self.action == 2:
+            
+            x = event.x
+            y = event.y
+
+            for node in self.graph.nodes:
+
+                if (abs(x - node.x) <= 10) and (abs(y - node.y) <= 10):
+                    node.delete(self.canvas)
+                    self.graph.delete_node( node )
+                    
+                    break
+
+            self.clear_widge()
+            self.action = 0
+        
+        elif self.action == 3:
+
+            x = event.x
+            y = event.y
+
+            cancel_ = True
+            for node in self.graph.nodes:
+                if (abs(x - node.x) <= 10) and (abs(y - node.y) <= 10):                    
+                    self.sel = node
+                    cancel_ = False                   
+                    break
+
+            if cancel_:
+                self.action = 0
+                self.clear_widge()
+
+            else:
+                self.action = 3.5
+                notice_label_2 = Label(self.window, font = ("Helvetica", 12), text = "Select second node:", fg = "blue", bg = "black")          
+                notice_label_2.grid(row = 3, column = 5)
+                self.upwidge.append(notice_label_2)
+        
+        elif self.action == 3.5:
+
+            x = event.x
+            y = event.y 
+
+            cancel_ = True
+            for node in self.graph.nodes:
+                if (abs(x - node.x) <= 10) and (abs(y - node.y) <= 10):                    
+                    self.don = node
+                    cancel_ = False                   
+                    break
+
+            if cancel_:
+                self.action = 0
+                self.clear_widge()
+                return (None)
+            else:
+                self.action = 3.5
+                notice_label_2 = Label(self.window, font = ("Helvetica", 12), text = "Select second node:", fg = "blue", bg = "black")          
+                notice_label_2.grid(row = 3, column = 5)
+                self.upwidge.append(notice_label_2)
+            
+            self.clear_widge()
+
+            in_weight = StringVar()
+
+            in_weight_label = Label(self.window, text = "Connection weight:", font = ("Helvetica", 12), fg = "blue", bg = "black")
+            in_weight_label.grid(row = 2, column = 5)
+            self.upwidge.append(in_weight_label)
+
+            in_weight_entry = Entry(self.window)
+            in_weight_entry.grid(row = 3, column = 5)
+            self.upwidge.append(in_weight_entry)
+
+            in_weight_button = Button(self.window, text = "Submit", command = lambda: in_weight.set( in_weight_entry.get() ) )
+            in_weight_button.grid(row = 4, column = 5)
+            self.upwidge.append(in_weight_button)
+                      
+            while True:
+
+                in_weight_button.wait_variable(in_weight) 
+                in_weight_label.grid_forget() 
+                con_weight = in_weight.get()
+
+                try:
+                    con_weight = float(con_weight)
+                    self.clear_widge()
+                    break
+                except:
+                    in_weight_label = Label(self.window, text = "Please enter a valid number", font = ("Helvetica", 15), fg = "blue", bg = "black")
+                    in_weight_label.grid(row = 2, column = 5)
+                    self.upwidge.append(in_weight_label)
+
+            self.graph.form_connection(self.don, self.sel, con_weight)
+
+            ##########
+
+            self.don = None
+            self.sel = None
+            self.upwidge = []
+        
+        elif self.action == 4:
+            pass
+
+    def build(self):
+        self.drnb.grid(row = 2, column = 1)
+        self.denb.grid(row = 3, column = 1)
+        self.drne.grid(row = 4, column = 1)
+        self.dene.grid(row = 5, column = 1)
+
+        self.canbut.grid(row = 2, column = 8)
+        self.delall.grid(row = 3, column = 8)
+
+        self.drnb.config(width = 10)
+        self.denb.config(width = 10)
+        self.drne.config(width = 10)
+        self.dene.config(width = 10)
+
+        self.canbut.config(width = 10)
+        self.delall.config(width = 10)
+        
+        self.canvas.bind("<Button-1>", self.user_action)
+        self.canvas.config(highlightbackground = "white", highlightthickness = 5)
+        self.canvas.grid(row = 6, column = 5)
+
+    def draw_node(self):
+        self.action = 1
+    
+    def delete_node(self):
+        self.action = 2
+    
+    def draw_edge(self):
+        self.action = 3
+
+        notice_label = Label(self.window,font = ("Helvetica", 15), text = "Select first node:", fg = "blue", bg = "black")          
+        notice_label.grid(row = 3, column = 5)
+        self.upwidge.append(notice_label)
+
+    def delete_edge(self):
+        self.action = 4
+    
+    def cancel_command(self):
+        self.sel = None
+        self.don = None
+        
+        for widget in self.upwidge:
+            widget.grid_forget()
+
+        self.action = 0
+    
+    def delete_all(self):
+        print(self.graph)
+        pass
+
+def open_window():
+
+    window = Tk()
+    window.title("Graphical Dijkstra's Algorithm")
+    window.configure(background = "black")
+    window.geometry("850x800")
+    
+    for row in range(60):
+        window.grid_rowconfigure(row, minsize = 10)
+    for col in range(60):
+        window.grid_columnconfigure(col, minsize = 10)
+
+    title = Label(window, text = "Dijkstra's Algorithm by Puria Radmard", font = ("Helvetica", 20), bg = "black", fg = "red")
+    title.grid(row = 1, column = 5)
+
+    run = DijkApp(window)
+    run.build()
+
+    window.mainloop()
+
+open_window()
